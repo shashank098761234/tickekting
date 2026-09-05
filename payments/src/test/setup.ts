3117d630272301f366f93cd725c2477da914939c
Shashank R <shashankr@Zeliots-MacBook-Air-4.local>
2026-09-05 16:06:40 +0530

    Initial commit

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -10,6 +10,8 @@ declare global {
 
 jest.mock("../nats-wrapper");
 
+process.env.STRIPE_KEY = "sk_test_hnfrAm8rOkryFEnV23jjfFlw";
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasdf";
